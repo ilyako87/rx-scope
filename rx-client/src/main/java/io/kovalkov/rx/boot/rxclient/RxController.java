00f30b4b0ce63bb8a37a62a69f4a59fd5ee6997f
Ilya Kovalkov <ilya.kovalkov@outletcity.com>
2022-02-18 15:13:01 +0100

    additions

--- rx-client/src/main/java/io/kovalkov/rx/boot/rxclient/RxController.java
+++ rx-client/src/main/java/io/kovalkov/rx/boot/rxclient/RxController.java
@@ -1,14 +1,21 @@
 package io.kovalkov.rx.boot.rxclient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
+@Slf4j
 @ComponentScan
 @RestController
 public class RxController {
@@ -22,19 +29,31 @@ public class RxController {
 	@Value("${rx-server.port}")
 	private String rxServerPort;
 
-	@GetMapping("/client-api")
+	@GetMapping("/rx-test")
 	public Mono<ResponseEntity<String>> monoTest() {
-		System.out.println(rxServerHostName);
-		System.out.println(rxServerPort);
+		log.debug(rxServerHostName);
+		log.debug(rxServerPort);
 		return googleClient.get()
 				.uri(uriBuilder ->
 						uriBuilder
 								.scheme("http")
 								.host(rxServerHostName)
 								.port(rxServerPort)
-								.pathSegment("server-api")
+								.pathSegment("server-rx-api")
 								.build())
 				.exchangeToMono(clientResponse -> clientResponse.toEntity(String.class));
+	}
+
+	@GetMapping("/standard-test")
+	public ResponseEntity<String> standardTest() {
+		final RestTemplate restTemplate = new RestTemplateBuilder().build();
+		final URI uri = new DefaultUriBuilderFactory().builder()
+				.scheme("http")
+				.host(rxServerHostName)
+				.port(rxServerPort)
+				.pathSegment("server-standard-api")
+				.build();
+		return restTemplate.getForEntity(uri, String.class);
 	}
 
 }
--- rx-server/src/main/java/io/kovalkov/rx/boot/rxserver/RxController.java
+++ rx-server/src/main/java/io/kovalkov/rx/boot/rxserver/RxController.java
@@ -2,6 +2,7 @@ package io.kovalkov.rx.boot.rxserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,15 @@ public class RxController {
 	@Autowired
 	private WebClient googleClient;
 
-	@GetMapping("/server-api")
+	@GetMapping("/server-rx-api")
 	public Mono<ResponseEntity<String>> monoTest() {
-		return googleClient.get().uri("https://www.google.com")
-				.exchangeToMono(clientResponse -> clientResponse.toEntity(String.class));
+		ResponseEntity<String> responseEntity = new ResponseEntity<>("Response from Server", HttpStatus.ACCEPTED);
+		return Mono.just(responseEntity);
+	}
+
+	@GetMapping("/server-standard-api")
+	public ResponseEntity<String> standardTest() {
+		return new ResponseEntity<>("Response from Server", HttpStatus.ACCEPTED);
 	}
 
 }
